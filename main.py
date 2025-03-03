288dc6a049b4b8d19332b027dbf6784e64d76e0a
snaghich <123023885+snaghich@users.noreply.github.com>
2025-03-03 09:39:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import my_oled
+import time
+state = 0
+while True:
+    print(state)
+    state = state+1
+    my_oled.print_text("something else",0,0)
+    time.sleep(1)
+if
+state = 0
+my_oled.print_text("test",0,0)
